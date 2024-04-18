b2f1383eaabb4fbf363f50e46c39727bf914af2d
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-18 21:52:06 +0100

    102

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 from typing import Tuple, List
 
-def zoom_array(lst: Tuple[int], factor: int = 2) -> Tuple[int]:
-    zoomed_in: List[int] = [
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    zoomed_in: List = [
         item for item in lst
-        for _ in range(factor)
+        for i in range(factor)
     ]
-    return tuple(zoomed_in)
+    return zoomed_in
 
 
 array = [12, 72, 91]
 
-zoom_2x = zoom_array(tuple(array))
+zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(tuple(array), 3)
+zoom_3x = zoom_array(array, 3)
