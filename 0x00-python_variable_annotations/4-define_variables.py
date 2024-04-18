99c6b131c4099f613b874bb5e1a61905c4d53abf
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-18 20:58:44 +0100

    4

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,1 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Define and annotate variables with specified values.
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
