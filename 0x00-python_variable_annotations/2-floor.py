984d66a9275504f3954e02eb56dd6a5906d6d18d
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-18 20:28:34 +0100

    2

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -4,13 +4,14 @@ import math
 
 def floor(n: float) -> int:
 
-    """
-    Return the floor of a float number.
 
-    Args:
-        n (float): The input float number.
+"""
+Return the floor of a float number.
 
-    Returns:
-        int: The floor of the input float.
-    """
-    return math.floor(n)
+Args:
+n (float): The input float number.
+
+Returns:
+int: The floor of the input float.
+"""
+return math.floor(n)
