989602e97c17a4bb146d132734886e51b3cd5cac
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-18 20:22:10 +0100

    1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,1 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+def concat(str1: str, str2: str) -> str:
+
+    """
+    Concatenate two strings.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenated string.
+    """
+    return str1 + str2
