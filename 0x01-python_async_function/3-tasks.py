359263dd4df9d32dd2a2442ffe7af8d79a99a687
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-22 08:11:48 +0100

    3

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,33 +1,19 @@
 #!/usr/bin/env python3
 
 import asyncio
-import random
-from typing import List
+from typing import Generator
 
-async def task_wait_random(max_delay: int) -> float:
+async def wait_random(max_delay: int) -> float:
     """
-    Asynchronous coroutine that waits for a
-    random delay between 0 and max_delay seconds.
-
-    Args:
-        max_delay (int): The maximum delay in seconds.
-
-    Returns:
-        float: The random delay waited.
+    Wait for a random delay between 0 and `max_delay` seconds.
     """
+    import random
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
 
-def task_wait_random_wrapper(max_delay: int) -> asyncio.Task:
+def task_wait_random(max_delay: int) -> Generator[asyncio.Task, None, None]:
     """
-    Wrapper function that creates and returns
-    an asyncio Task for task_wait_random.
-
-    Args:
-        max_delay (int): The maximum delay in seconds.
-
-    Returns:
-        asyncio.Task: An asyncio Task object.
+    Create and return an asyncio.Task for the wait_random coroutine.
     """
-    return asyncio.create_task(task_wait_random(max_delay))
+    return asyncio.create_task(wait_random(max_delay))
