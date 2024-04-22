5c49c908434d749884181b138498012bc319322d
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-22 08:23:17 +0100

    all

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -3,6 +3,7 @@
 import asyncio
 from typing import Generator
 
+
 async def wait_random(max_delay: int) -> float:
     """
     Wait for a random delay between 0 and `max_delay` seconds.
@@ -11,6 +12,7 @@ async def wait_random(max_delay: int) -> float:
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
+
 
 def task_wait_random(max_delay: int) -> Generator[asyncio.Task, None, None]:
     """
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -5,6 +5,7 @@ from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Asynchronous function that spawns task_wait_random
