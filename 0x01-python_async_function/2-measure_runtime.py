42f64b1246ff44f2f6df02c9c545efca2ae97fd1
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-23 08:12:20 +0100

    2

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+"""
+Module: 2-measure_runtime
+
+Contains a function to measure the runtime of the wait_n coroutine.
+"""
+
 import asyncio
 import time
 from typing import Callable
