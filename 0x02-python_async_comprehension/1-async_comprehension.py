06695f8320ae8cb92242a0f39fdd56a50e93c50c
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-23 08:40:27 +0100

    all

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Contains async_comprehension coroutine
+"""
+
+from typing import List
+
+from random import uniform
+from asyncio import gather
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Collects 10 random numbers using async comprehension over async_generator.
+    Returns a list of the 10 random numbers.
+    """
+    return [i async for i in async_generator()]
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,22 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Contains measure_runtime coroutine
+"""
+
+import asyncio
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Executes async_comprehension four times in parallel using asyncio.gather.
+    Measures the total runtime and returns it.
+    """
+    start_time = asyncio.get_event_loop().time()
+
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
+
+    end_time = asyncio.get_event_loop().time()
+
+    return end_time - start_time
